485781ace843719bff8f708e217aaaec0145d719
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-09-20 14:55:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use('/api/v1/product',productRoutes);
 //     })
 // })
 app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    res.sendFile(path.join(__dirname,'./client/build/static/index.html'))
 })
 
 const PORT=process.env.PORT || 8080;
